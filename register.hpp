1e178998cbf98a49bca4603ea4f3f470ff0bb0ca
Water Chika <water_chika@outlook.com>
2025-09-01 18:49:32 +0000

    Add register get helper

---
+++ register.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <hip/hip_runtime.h>
+
+namespace hip_helper{
+
+template<uint32_t HW_REG_INDEX>
+__device__
+uint32_t get_reg_value() {
+    uint32_t v;
+    asm("s_getreg_b32 %0, hwreg(%1, 0, 32)" : "=s"(v) : "i"(HW_REG_INDEX));
+    return v;
+}
+
+template<uint32_t V, uint32_t... Vs>
+struct parameters_call {
+    __device__
+    static void call(uint32_t* out) {
+        out[V] = get_reg_value<V>();
+        parameters_call<Vs...>::call(out);
+    }
+};
+template<uint32_t V>
+struct parameters_call<V> {
+    __device__
+    static void call(uint32_t* out) {
+        out[V] = get_reg_value<V>();
+    }
+};
+
+template<uint32_t I, uint32_t... Indices>
+__global__
+void device_get_reg_value(uint32_t* out) {
+    parameters_call<I, Indices...>::call(out);
+}
+
+template<uint32_t N, uint32_t... Ns>
+auto get_reg_values() {
+    constexpr auto reg_indices = std::array{N, Ns...};
+    auto reg_values = std::array<uint32_t, reg_indices.size()>{};
+    {
+        auto v = hip_helper::device::array<uint32_t, reg_values.size()>{};
+        device_get_reg_value<N, Ns...><<<dim3(1,1,1),dim3(1,1,1),0>>>(v.data());
+        reg_values = v;
+    }
+    return std::pair{reg_indices, reg_values};
+}
+
+}
