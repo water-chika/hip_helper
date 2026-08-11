d73514c682a9096a888a0a12090b84caa7d1ccca
Water Chika <water_chika@outlook.com>
2026-08-11 21:18:59 +0800

    update

--- build.hpp
+++ build.hpp
@@ -2,8 +2,9 @@
 
 namespace hip_helper_build {
 
-void add(build::builder& builder, std::filesystem::path path) {
-    builder.add_library("hip_helper", absolute(path).string(), "/opt/rocm/include");
+void add(build::builder& builder, std::filesystem::path path, std::filesystem::path build_dir) {
+    builder.add_library("hip_helper", build_dir / "hip_helper",
+            absolute(path).string(), "/opt/rocm/include");
 }
 
 }
