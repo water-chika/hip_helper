8f56e37044292b20ca62366d5592c2470782fd1c
Water Chika <water_chika@outlook.com>
2026-08-14 17:56:56 +0800

    add_external_memory

--- hip_helper.hpp
+++ hip_helper.hpp
@@ -5,6 +5,8 @@
 
 namespace hip_helper{
 
+using cpp_helper::configure;
+
 template<typename T, typename... Params>
 struct hybrid_call {
     using Result = std::invoke_result_t<decltype(T::invoke), Params...>;
@@ -33,6 +35,80 @@ struct hybrid_call {
     }
 
     Result m_res;
+};
+
+template<typename T>
+class add_external_memory : public T {
+public:
+    using parent = T;
+    add_external_memory(const configure auto& conf) : parent{conf},
+        external_memory{}
+    {
+        create();
+    }
+    ~add_external_memory() {
+        destroy();
+    }
+    auto get_external_memory_size() {
+        return parent::get_dma_buf_size();
+    }
+    void create() {
+        auto dma_buf_fd = parent::get_dma_buf_fd();
+        auto dma_buf_size = parent::get_dma_buf_size();
+        hipExternalMemoryHandleDesc desc = {};
+        desc.size = dma_buf_size;
+        desc.type = hipExternalMemoryHandleTypeOpaqueFd;
+        desc.handle.fd = dma_buf_fd;
+        int ret = hipImportExternalMemory(&external_memory, &desc);
+        if (ret != hipSuccess) {
+            throw std::runtime_error{std::format("hipImportExternalMemory failed: {}", ret)};
+        }
+    }
+    void destroy() {
+        int ret = hipDestroyExternalMemory(&external_memory);
+        if (ret != hipSuccess) {
+            throw std::runtime_error{std::format("hipDestroyExternalMemory failed: {}", ret)};
+        }
+    }
+    auto get_external_memory() {
+        return external_memory;
+    }
+private:
+    hipExternalMemory_t external_memory;
+};
+
+template<typename T>
+class add_external_memory_buffer : public T {
+public:
+    using parent = T;
+    add_external_memory_buffer(const configure auto& conf) : parent{conf},
+        buffer{}
+    {
+        create();
+    }
+    ~add_external_memory_buffer() {
+        destroy();
+    }
+    void create() {
+        hipExternalMemoryBufferDesc desc = {};
+        desc.offset = 0;
+        desc.size = parent::get_external_memory_size();
+        desc.flags = 0;
+
+        auto external_memory = parent::get_external_memory();
+
+        int ret = hipExternalMemoryGetMappedBuffer(reinterpret_cast<void**>(&buffer), external_memory, &desc);
+        if (ret != hipSuccess) {
+            throw std::runtime_error{std::format("hipExternalMemoryGetMappedBuffer failed: {}", ret)};
+        }
+    }
+    void destroy() {
+    }
+    auto get_external_memory_buffer() {
+        return buffer;
+    }
+private:
+    void* buffer;
 };
 
 }
